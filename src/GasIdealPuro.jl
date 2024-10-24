1dfed16230ff2acf1e330e6a68c8c5dd9b594fcc
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-10-24 17:33:12 -0300

    UPD the src/GasIdealPuro.jl

--- src/GasIdealPuro.jl
+++ src/GasIdealPuro.jl
@@ -1,8 +1,13 @@
 module GasIdealPuro
 
-# Write your package code here.
+# Imports
 
-struct modelo_cp_const <: Any
+using EngThermBase
+
+struct modelo_cp_const <: Heat{Float64, EX}
+    M::m_amt{Float64, EX, MO}
+    c::cpamt{Float64, EX, MO}
+    # TODO: Completar!!
 end
 
 end
